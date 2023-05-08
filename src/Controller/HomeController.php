6031e08833ebb66b5cafb0a7f07df27dd22bb8c1
Marcin Malessa <marcin@malessa.pl>
2023-05-08 21:45:03 +0200

    Add HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class HomeController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return new Response('My beautiful homepage');
+    }
+}
